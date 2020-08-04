33fe09868efb977a644a5a9647f8af9694e8d6b2
duydinhnguyen <57514892+duydinhnguyen@users.noreply.github.com>
2020-08-04 15:25:53 +0700

    update fb comment 2

--- 1.js
+++ 1.js
@@ -1,13 +1,14 @@
 ﻿ $(function(){
      vitricuaedu=$('#khoiedu').offset().top;
      vitricuasp=$('#khoisp').offset().top;
+     vitricuacomment=$('#phanhoi').offset().top;
      
      $(".xuong,.kn").click(function(){
          $('body,html').animate({ scrollTop:vitricuaedu },700)
          return false;
      })
-     $(".sp").click(function(){
-        $('body,html').animate({ scrollTop:vitricuasp },700)
+     $(".cm").click(function(){
+        $('body,html').animate({ scrollTop:vitricuacomment},700)
         return false;
     })
  
