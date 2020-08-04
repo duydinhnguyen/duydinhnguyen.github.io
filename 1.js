a4802dad5a56b02984ed5e0ab71deb006b47d474
duydinhnguyen <57514892+duydinhnguyen@users.noreply.github.com>
2020-08-04 13:50:25 +0700

    update css

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
 ﻿ $(function(){
+     vitricuaedu=$('#khoiedu').offset().top;
+     vitricuasp=$('#khoisp').offset().top;
+     
+     $(".xuong,.kn").click(function(){
+         $('body,html').animate({ scrollTop:vitricuaedu },700)
+         return false;
+     })
+     $(".sp").click(function(){
+        $('body,html').animate({ scrollTop:vitricuasp },700)
+        return false;
+    })
  
 })  
  
