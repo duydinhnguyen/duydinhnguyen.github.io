a1272f57e1c8b8830562cdb2b42ed1e772ca17a7
duydinhnguyen <57514892+duydinhnguyen@users.noreply.github.com>
2020-08-05 16:30:02 +0700

    xong

--- 1.js
+++ 1.js
@@ -1,8 +1,15 @@
-﻿ $(function(){
+﻿
+ $(function(){
+     vitrikhoigt=$('#khoigt').offset().top;
      vitricuaedu=$('#khoiedu').offset().top;
      vitricuasp=$('#khoisp').offset().top;
      vitricuacomment=$('#phanhoi').offset().top;
+     vitricuacontact=$('#lienhe').offset().top;
      
+     $(".gt,.navbar-brand").click(function(){
+        $('body,html').animate({ scrollTop:vitrikhoigt},700)
+        return false;
+    })
      $(".xuong,.kn").click(function(){
          $('body,html').animate({ scrollTop:vitricuaedu },700)
          return false;
@@ -11,6 +18,40 @@
         $('body,html').animate({ scrollTop:vitricuacomment},700)
         return false;
     })
- 
+    $(".sp").click(function(){
+        $('body,html').animate({ scrollTop:vitricuasp},700)
+        return false;
+    })
+
+    $(".contact").click(function(){
+        $('body,html').animate({ scrollTop:vitricuacontact},700)
+        return false;
+    })
+
+
+    /* xử lý sự kiện cho skill bar */
+    $(window).on ('load', function (){ // makes sure the whole site is loaded
+
+        // ----------------------- 
+      // Progress Bar--------------------
+      // 
+  $('.progress-bar').each(function(){
+          var width = $(this).data('percent');
+          $(this).css({'transition': 'width 3s'});
+          $(this).appear(function() {
+              console.log('hello');
+              $(this).css('width', width + '%');
+              $(this).find('.count').countTo({
+                  from: 0,
+                  to: width,
+                  speed: 3000,
+                  refreshInterval: 50
+              });
+          });
+      });
+
+
+  });  //End On Load Function
+  /*End xử lý sự kiện cho skill bar */
 })  
  
